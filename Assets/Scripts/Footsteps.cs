6d0abe9c0b19713fe9552af2d5eda696e51cc81d
Gary Chang <gary.chang.dev@gmail.com>
2020-08-14 15:10:50 -0400

    Implemented 3D and 2D audio test scenes

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    private PlayerController m_PlayerController;
+    
+    [SerializeField]
+    private AudioSource m_footStepSound;
+
+    private void Start()
+    {
+        m_PlayerController = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        if (m_PlayerController.GetMoveDirection().magnitude >= 0.1f)
+        {
+            if (!m_footStepSound.isPlaying)
+            {
+                m_footStepSound.Play();
+            }
+        }
+    }
+}
